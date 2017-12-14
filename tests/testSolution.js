ef927d9c87c266d7713d8d23215415ddaea6d334
Mike Palmer <mike.palmer@workplacedynamics.com>
2017-12-14 09:58:52 -0500

    Add option to testSolution function to specify timeout

--- tests/testSolution.js
+++ tests/testSolution.js
@@ -1,10 +1,13 @@
 const { expect } = require('chai')
 
-module.exports = function testSolution({ solution, inputs, extraArguments = [], expectedOutputs }) {
+module.exports = function testSolution({ solution, inputs, extraArguments = [], expectedOutputs, timeout }) {
     expect(inputs).to.have.lengthOf(expectedOutputs.length)
 
     inputs.forEach((input, index) => {
-        it(`should output "${expectedOutputs[index]}" for input "${input}"`, () => {
+        it(`should output "${expectedOutputs[index]}" for input "${input}"`, function () {
+            if (timeout) {
+                this.timeout(timeout)
+            }
             expect(solution(input, ...extraArguments)).to.equal(expectedOutputs[index])
         })
     })
